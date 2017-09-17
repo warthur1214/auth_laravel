6ca33751e3a532e8283b5228b374718e0b3f7a87
warthur1214 <warthur@163.com>
2017-09-17 22:18:05 +0800

    调整

--- app/Http/Dao/AccountDAO.php
+++ app/Http/Dao/AccountDAO.php
@@ -11,7 +11,6 @@ namespace App\Http\Dao;
 
 use App\Http\Dao\inter\AbstractRepository;
 use App\Http\Dao\inter\BaseDAO;
-use DB;
 use Exception;
 
 class AccountDAO extends AbstractRepository implements BaseDAO
@@ -23,11 +22,7 @@ class AccountDAO extends AbstractRepository implements BaseDAO
     {
         try {
             return $this->db()->where(['a.account_id'=>$account->account_id])
-                ->leftJoin(
-                    't_account_role_rel as ar',
-                    'a.account_id',
-                    '=',
-                    'ar.account_id'
+                ->leftJoin('t_account_role_rel as ar', 'a.account_id', '=', 'ar.account_id'
                 )->pluck('role_id');
         } catch (\Exception $e) {
             throw new Exception($e);
